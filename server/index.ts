8bcfdd6b0643c5500e0f71ffea7c646ecea7eeb2
deicide <janmanuelmurillo32@gmail.com>
2025-07-19 17:27:39 +0800

    bago to perd

--- server/index.ts
+++ server/index.ts
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen(port, () => {
+  server.listen(port, () => { //new port listener
     log(`serving on port ${port}`);
   });
 })();
